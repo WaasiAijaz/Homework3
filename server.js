476ec5cc647c0ca813c35fb2949cfb9989916ec8
WaasiAijaz <waasiaijaz786@gmail.com>
2025-11-11 19:46:32 -0500

    Created all posts section

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.get('/', (req, res) =>{
     console.log('Here');
-    res.send('All Posts');
-    
+    res.render("index", {post: "Posts"})
 });
+app.get('/post/all',(req,res)=>{
+    res.send('All posts page')
+} )
 app.listen(3030);
