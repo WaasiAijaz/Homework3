0f11ea520310b47609c21b72eb0a9fa3400d69ed
WaasiAijaz <waasiaijaz786@gmail.com>
2025-11-11 19:32:58 -0500

    Installed ejs, setup html page, and sent basic welcome message

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 app.get('/', (req, res) =>{
+    console.log('Here');
+    res.render("index", {post:"Posts"});
     
 });
 app.listen(3030);
