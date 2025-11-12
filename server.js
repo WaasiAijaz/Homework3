8439464d3d5a2f0dd024043ef669fcde0f8a5264
WaasiAijaz <waasiaijaz786@gmail.com>
2025-11-11 19:49:22 -0500

    Created post file and moved code into that file

--- server.js
+++ server.js
@@ -5,7 +5,4 @@ app.get('/', (req, res) =>{
     console.log('Here');
     res.render("index", {post: "Posts"})
 });
-app.get('/post/all',(req,res)=>{
-    res.send('All posts page')
-} )
 app.listen(3030);
---
+++ views/posts.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router;
+app.get('/post/all',(req,res)=>{
+    res.send('All posts page')
+});
